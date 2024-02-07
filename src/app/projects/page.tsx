8992cefb362bcead3c757bdec430e77f30e1702d
rizeenf <rize.nf11@gmail.com>
2024-02-07 10:52:36 +0700

    finishing

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -16,43 +16,51 @@ import {
 const PROJECTS = [
   {
     id: 1,
-    link: "rize-socialmedia.vercel.app",
-    img: "/projChatapp2.png",
-    name: "MWatsapp | Chat Apps",
-    desc: "Chat application build using React for UI and Firebase for handling Auth and Storage (Firestore)",
-    framework: "React, Tailwind, Firebase Auth, Firestore",
-    demo: "https://rize-chatreact-firebase.vercel.app/",
-    src: "https://github.com/rizeenf/chat-react-firebase",
+    link: "myjfest.up.railway.app",
+    img: "/projJfest1.png",
+    img2: "/projJfest2.png",
+    img3: "/projJfest3.png",
+    name: "MyJfest | Ditigal Marketplace",
+    desc: "MyJFest is a marketplace platform created for searching any Japanese events, we can see event details and buy event ticket if exist, it also build for Costume-maker sell their products in this platform.",
+    framework: "Next.js, React, Tailwind, Redux, Stripe",
+    demo: "http://myjfest.up.railway.app/",
+    src: "https://github.com/rizeenf/jfest",
     color: "from-blue-50 to-orange-50",
   },
   {
     id: 2,
+    link: "rize-chatreact-firebase.vercel.app",
+    img: "/projChatapp2.png",
+    img2: "/projChatapp2.png",
+    img3: "/projChatapp2.png",
+    name: "MWatsapp | Chat Apps",
+    desc: "Chat Application build for an online chatting application, it allows you to have a conversation in real time, it can also have a image sharing option. Build using React for UI and Firebase for handling Auth and Storage (Firestore)",
+    framework: "React, Tailwind, Firebase Auth, Firestore",
+    demo: "https://rize-chatreact-firebase.vercel.app/",
+    src: "https://github.com/rizeenf/chat-react-firebase",
+    color: "from-orange-50 to-violet-50",
+  },
+  {
+    id: 3,
     link: "rize-socialmedia.vercel.app",
     img: "/projSocialmed2.jpeg",
+    img2: "/projSocialmed2.jpeg",
+    img3: "/projSocialmed2.jpeg",
     name: "Sipaling social | Socialmedia",
     desc: "Mini social media that you can upload, post, like, comment, and share such as usual social media",
     framework: "React, Tailwind",
     demo: "https://rize-socialmedia.vercel.app/",
     src: "https://github.com/rizeenf/socialmedia",
-    color: "from-orange-50 to-violet-50",
-  },
-  {
-    id: 3,
-    link: "rize-shoprizing.vercel.app",
-    img: "/projShoprizing2.jpeg",
-    name: "Shoprizing | Marketplace",
-    desc: "Shopping application using Image API from Pixabay, it also using Redux for storing Cart",
-    framework: "React, Redux, Sass",
-    demo: "https://rize-shoprizing.vercel.app/",
-    src: "https://github.com/rizeenf/shoprizing",
     color: "from-violet-50 to-red-50",
   },
   {
     id: 4,
     link: "https://zashboard.vercel.app",
     img: "/projZashboard2.png",
+    img2: "/projZashboard2.png",
+    img3: "/projZashboard2.png",
     name: "Zashboard | Admin dashboard",
-    desc: "Dashboard provides charts for every data collected, using Charts from recharts and Data Table from MaterialUI",
+    desc: "Zashboard build for manage administration tool design for simplify data management. It transforms complex reports into visually appealing charts and tables for efficiently managing lists of items. Build using Charts from recharts and Data Table from MaterialUI",
     framework: "React, Sass, MaterialUI, Recharts",
     demo: "https://zashboard.vercel.app",
     src: "https://github.com/rizeenf/admindashboard",
@@ -108,10 +116,10 @@ const Portfolio = () => {
                           <Image src={item.img} alt={item.name} fill />
                         </CarouselItem>
                         <CarouselItem className="w-80 relative md:w-96 lg:w-[500px] xl:w-[600px] aspect-video ">
-                          <Image src={item.img} alt={item.name} fill />
+                          <Image src={item.img2} alt={item.name} fill />
                         </CarouselItem>
                         <CarouselItem className="w-80 relative md:w-96 lg:w-[500px] xl:w-[600px] aspect-video ">
-                          <Image src={item.img} alt={item.name} fill />
+                          <Image src={item.img3} alt={item.name} fill />
                         </CarouselItem>
                       </CarouselContent>
                       <CarouselPrevious />
